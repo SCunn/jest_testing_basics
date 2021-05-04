95c5a70f5a5d9518bdeacb1c8eea76c100128e18
scunn <shanecunn@hotmail.com>
2021-05-04 18:29:14 +0100

    Jest method examples

--- functions.js
+++ functions.js
@@ -1,5 +1,20 @@
+const { default: axios } = require("axios");
+
 const functions = {
-    add : (num1, num2) => num1 + num2
-}
+    add : (num1, num2) => num1 + num2,
+    isNull: () => null,
+    // isNull: () => undefined
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Shane' }
+        user['lastName'] = 'Cunningham';
+        return user;
+    },
+    fetchUser: () => 
+        axios
+        .get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
+        .catch(err => 'error')
+};
     
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,77 @@
 const functions = require('./functions');
 
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull : matches only null
+// toBeUndefined : matches only undefined
+// toBeDefined :  is the opposite of toBeUndefined
+// toBeTruthy : matches anything when an if statement is true
+// toBeFalsy : matches anything when an if statement is false
+
+
 test('Adds 2 + 2 to equal 4', () => {
    expect(functions.add(2, 2)).toBe(4); 
+});
+
+// toBe
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5); 
+ });
+
+ // toBeNull
+ test('Should be null', () => {
+    expect(functions.isNull()).toBeNull(); 
+ });
+
+ // toBeFalsy
+ test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy(); 
+ });
+
+ // toEqual
+ test('User should be Shane Cunningham object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Shane', lastName: 'Cunningham'
+    }); 
+ })
+
+ // Less than and greater than.  Logic called from within test function, not functions.js file.
+ test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+ });
+
+ test('Should be under or equal to 1600', () => {
+    const load1 = 800;
+    const load2 = 800;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+ });
+
+ // Regex
+ test('There is no I in team', () => {
+     expect('team').not.toMatch(/I/);
+ });
+
+ // Arrays
+ test('Admin should be in usernames', () => {
+     usernames = ['john', 'karen', 'admin'];
+     expect(usernames).toContain('admin');
+ });
+
+ // Working with async data
+
+ // Promise
+//  test('User fetched name should be Leanne Graham', () => {
+//      expect.assertions(1);
+//      return functions.fetchUser()
+//      .then(data => {
+//          expect(data.name).toEqual('Leanne Graham');
+//      })
+//  });
+
+ // Async Await
+ test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser()
+    expect(data.name).toEqual('Leanne Graham');
 });
